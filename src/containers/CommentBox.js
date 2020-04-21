596b476f0cdad81aabc76ad645cb2bce155b18e9
cowannoel <cowannoel@gmail.com>
2020-04-21 13:22:45 +0100

    complete comments

---
+++ src/containers/CommentBox.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import CommentList from '../components/CommentList';
+import CommentForm from '../components/CommentForm';
+
+class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: [
+        {
+          id: 1,
+          author: "Beth Fraser",
+          text: "I love JS more each day!"
+        },
+        {
+          id: 2,
+          author: "Sky Su",
+          text: "Wait until we add the form - it's gonna be so good!"
+        }
+      ]
+    }
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+  }
+
+  handleCommentSubmit(submittedComment){
+    submittedComment.id = Date.now();
+    const updatedComments = [...this.state.comments, submittedComment]
+    this.setState({
+      comments: updatedComments
+    })
+  }
+
+  render() {
+    return (
+      <div className="comment-box">
+        <h2>Add a Comment!</h2>
+        <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
+        <h2>Comments:</h2>
+        <CommentList comments={this.state.comments} />
+      </div>
+    )
+  }
+}
+export default CommentBox;
